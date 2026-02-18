51f62d793b3a2f883455bd33f79037dbf3703268
Pascal Charbonneau <pcharbon70@gmail.com>
2026-02-18 07:17:31 -0700

    Phase 2: add markdown workflow parser and loader

---
+++ lib/jido_workflow/workflow/loader.ex
@@ -0,0 +1,26 @@
+defmodule JidoWorkflow.Workflow.Loader do
+  @moduledoc """
+  Loads markdown workflow definitions and validates them into typed contracts.
+  """
+
+  alias JidoWorkflow.Workflow.Definition
+  alias JidoWorkflow.Workflow.MarkdownParser
+  alias JidoWorkflow.Workflow.ValidationError
+  alias JidoWorkflow.Workflow.Validator
+
+  @spec load_file(Path.t()) :: {:ok, Definition.t()} | {:error, [ValidationError.t()]}
+  def load_file(path) when is_binary(path) do
+    with {:ok, parsed} <- MarkdownParser.parse_file(path),
+         {:ok, definition} <- Validator.validate(parsed) do
+      {:ok, definition}
+    end
+  end
+
+  @spec load_markdown(binary()) :: {:ok, Definition.t()} | {:error, [ValidationError.t()]}
+  def load_markdown(markdown) when is_binary(markdown) do
+    with {:ok, parsed} <- MarkdownParser.parse(markdown),
+         {:ok, definition} <- Validator.validate(parsed) do
+      {:ok, definition}
+    end
+  end
+end
